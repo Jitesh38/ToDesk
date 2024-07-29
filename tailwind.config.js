cdc5780fa2e9d943e2946c02e7b55bd58f2a2179
Jitesh38 <jp2112003@gmial.com>
2024-07-29 14:50:03 +0530

    first committe

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content:["*.html"],
+  theme: {
+    extend: {
+      colors:{
+        'primary':'#3238f2'
+      },
+      fontFamily:{
+        'poppins':['Poppins','sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
